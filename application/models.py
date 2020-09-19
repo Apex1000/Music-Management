224ee6352300aa3c20fbe218b581438da4d5fdc8
apex1000 <siddhartsingh9876@gmial.com>
2020-09-19 15:08:16 +0530

    slug = db.Column(

--- application/models.py
+++ application/models.py
@@ -1,2 +1,41 @@
 from . import db
 
+class Songs(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    title = db.Column(
+        db.String(64),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+    artist = db.Column(
+        db.String(64),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+    album = db.Column(
+        db.String(64),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+    slug = db.Column(
+        db.String(64),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+    slug = db.Column(
+        db.String(64),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+    
+    def __repr__(self):
+        return '<Todo {}>'.format(self.todo)
